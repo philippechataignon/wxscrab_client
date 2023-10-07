0cb49a66a35bdbe2284992afc54e79f569512a5e
Username <user@example.com>
2023-10-07 10:15:02 +0200

    Version dsp ubuntu 22.04

--- son.py
+++ son.py
@@ -5,22 +5,33 @@ import wx.adv
 class son :
     def __init__(self) :
         self.sons = {
-            'debut':wx.adv.Sound("sound/debut.wav"),
-            'fin_tour':  wx.adv.Sound("sound/fin_tour.wav"),
-            'valid':wx.adv.Sound("sound/valid.wav")
+            'debut':self.read_wav("sound/debut.raw"),
+            'fin_tour': self.read_wav("sound/fin_tour.raw"),
+            'valid': self.read_wav("sound/valid.raw")
         }
 
     def play_debut(self, m) :
-        self.sons['debut'].Play(wx.adv.SOUND_ASYNC)
+        self.play_dsp(self.sons['debut'])
         return m
 
     def play_valid(self, m) :
-        self.sons['valid'].Play(wx.adv.SOUND_ASYNC)
+        self.play_dsp(self.sons['valid'])
         return m
 
     def play_fin_tour(self, m) :
-        self.sons['fin_tour'].Play(wx.adv.SOUND_ASYNC)
+        self.play_dsp(self.sons['fin_tour'])
         return m
+
+    def play_dsp(self, data):
+        out = open("/dev/dsp", "wb")
+        out.write(data)
+        out.close()
+
+    def read_wav(self, name):
+        f = open(name, "rb")
+        ret = f.read()
+        f.close()
+        return ret
 
 if __name__ == '__main__':
     s = son()
